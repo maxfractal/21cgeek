739ad12f38c439aec3e196b43762812d709d63e0
Xris <cp@maxfractal.com>
2018-07-09 17:13:52 -0400

    Adding Invision Projects

---
+++ xris/ux/projects/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Projects</title>
+</head>
+<body>
+<div>
+    <?php
+    echo '<p>Projects Landing</p>';
+    echo 'This is a PHP test.'
+    echo '<p>ISLT Enhanced</p>';
+    ?>
+    <p><a href="case_studies/wireframes/etouches/eselect/index.html">eSelect Demo</a></p>
+</div>
+</body>
+</html>
