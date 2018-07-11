3ea029aab165b79378f7a4be99f3c971c684210f
Xris <cp@maxfractal.com>
2018-07-10 23:05:08 -0400

    Changes to the UX projects index

--- xris/ux/projects/index.php
+++ xris/ux/projects/index.php
@@ -8,9 +8,10 @@
 <div>
     <?php
     echo '<p>Projects Landing</p>';
-    echo 'This is a PHP test.'
-    echo '<p>ISLT Enhanced</p>';
+    echo 'This is a PHP test.';
+    echo 'ISLT Enhanced';
     ?>
+    <p><a href="case_studies/wireframes/ISLT/index.html">ISLT Enhanced</a></p>
     <p><a href="case_studies/wireframes/etouches/eselect/index.html">eSelect Demo</a></p>
 </div>
 </body>
