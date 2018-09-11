c6e71cb65208ef7eaec2a545f042b59a9f5f9c1f
Xris <cp@maxfractal.com>
2018-09-11 11:06:10 -0400

    UX project work

---
+++ includes/includes.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$home = site_url('/');
+$xris = site_url('/xris/projects');
+$xCSS = site_url('/css');
+        
+
+?>
