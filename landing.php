57423e427723769d1ab4a55951b4bf576a2db11a
Xris <cp@maxfractal.com>
2019-01-05 22:25:04 -0500

    Quiz and portfolio landing page

--- landing.php
+++ landing.php
@@ -13,6 +13,14 @@
 </head>
 
 <body>
+	<main>
+		<section>
+			<?php 
+			
+			$currentUser =wp_get_current_user(); 
+			echo 'Current user = ' . $currentUser->user_login . '<br>';
+			?>
+		</section>
     <section>
         <?php echo site_url('/xris'); ?><br>
         <?php echo $home; ?><br>
@@ -25,6 +33,7 @@
 		<h4><?php echo(bloginfo('description')); ?></h4>
 	</article>
 </section>
+</main>
 	<?php 
 		while(have_posts()) {
 			the_post(); ?>
